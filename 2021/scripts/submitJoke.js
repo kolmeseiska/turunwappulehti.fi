be922a5716a8189c5303a2cf0306c529c633f17a
niemisami <sami.nieminen@madbooster.com>
2022-03-17 19:53:51 +0200

    Move last years site under 2021 folder

---
+++ 2021/scripts/submitJoke.js
@@ -0,0 +1,63 @@
+var apiUrl = 'https://ay6i44yayb.execute-api.eu-north-1.amazonaws.com/production/';
+
+var formEl = document.getElementById('joke-form');
+var submitButton = document.getElementById('submit-button');
+var loadingAnimation = document.getElementById('loading-animation');
+
+function validate() {
+  submitButton.disabled = formEl[0].value === '' || formEl[1].value === '' || formEl[2].value === '';
+}
+
+document.getElementById('name-field').addEventListener('keyup', validate);
+document.getElementById('email-field').addEventListener('keyup', validate);
+document.getElementById('joke-field').addEventListener('keyup', validate);
+
+if (formEl) {
+  formEl.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    submitButton.disabled = true;
+    loadingAnimation.className = 'loading';
+    
+    var body = [
+      formEl[0].value,
+      formEl[1].value,
+      formEl[2].value
+    ];
+
+    owlFlight();
+
+    fetch(apiUrl, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    })
+    .then((res, err) => {
+      if (err) console.log(err);
+      return res.json();
+    })
+    .then(data => {
+      loadingAnimation.className = 'loading hidden';
+      
+      formEl[0].value = formEl[1].value = formEl[2].value = '';
+      
+      if (data.statusCode !== 200) {
+        alert('Jotain meni nyt mönkään');
+      }
+    });
+  });
+}
+
+function owlFlight() {
+  var owl = document.getElementById('superbowl');
+  if (!owl) {
+    console.log("pöllö on kiireinen"); return;
+  }
+  owl.className = "fly";
+  setTimeout(() => {
+    owl.className = "";
+  }, 5000);
+}
