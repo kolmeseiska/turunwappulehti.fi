3187557bac2c392ac546ed973785ef1d4992a290
niemisami <sami.nieminen@madbooster.com>
2022-03-21 09:11:48 +0200

    Add form validation, error messages and confirmation

--- scripts/submitJoke.js
+++ scripts/submitJoke.js
@@ -1,30 +1,33 @@
 var apiUrl = 'https://92zu8pndu0.execute-api.eu-north-1.amazonaws.com/default/wappulehti-append-joke'
 
-var formEl = document.getElementById('joke-form')
+var formElement = document.getElementById('joke-form')
 var submitButton = document.getElementById('submit-button')
+var errorNotification = document.getElementById('errors')
+var errorMessages = document.getElementById('error-messages')
 var loadingAnimation = document.getElementById('loading-animation')
 
 function validate () {
-  submitButton.disabled = formEl[0].value === '' || formEl[1].value === '' || formEl[2].value === '' || formEl[3].value === ''
+  const jokeField = formElement[0]
+  submitButton.disabled = jokeField.value?.trim() === ''
 }
 
-// document.getElementById('name-field').addEventListener('keyup', validate)
-// document.getElementById('email-field').addEventListener('keyup', validate)
-// document.getElementById('joke-field').addEventListener('keyup', validate)
-// document.getElementById('fuksi-field').addEventListener('keyup', validate)
+document.getElementById('joke').addEventListener('keyup', validate)
 
-if(formEl) {
-  formEl.addEventListener('submit', function(event) {
+if(formElement) {
+  formElement.addEventListener('submit', function(event) {
     event.preventDefault()
 
-    submitButton.disabled = false
-    loadingAnimation.className = 'loading'
+    submitButton.disabled = true
+    clearErrors()
+    const loadingTimeout = setTimeout(() => {
+      submitButton.innerHTML = 'Lähetetään...'
+    }, 600)
 
     var body = {
-      joke: formEl[0].value,
-      email: formEl[1].value,
-      guild: formEl[2].value || 'Muu',
-      isFuksi: formEl[3].checked
+      joke: formElement[0].value,
+      email: formElement[1].value,
+      guild: formElement[2].value || 'Muu',
+      isFuksi: formElement[3].checked
     }
 
     fetch(apiUrl, {
@@ -36,19 +39,46 @@ if(formEl) {
       body: JSON.stringify(body)
     })
       .then((res, err) => {
-        if(err) console.log(err)
+        if(loadingTimeout) {
+          clearInterval(loadingTimeout)
+        }
         if(res.status !== 200 && res.status !== 400) {
           alert('Pahoittelut! Jotain meni pieleen')
+          if(err) {
+            console.log(err)
+          }
         }
         return res.json()
       })
-      .then(data => {
-        // TODO: validation joke can be max 3000 chars long etc
-        console.log('Response', data)
-        loadingAnimation.className = 'loading hidden'
-
-        formEl[0].value = formEl[1].value = formEl[2].value = ''
-        formEl[3].checked = false
+      .then(data => {       
+        if(data.errors) {
+          console.warn(data)
+          submitButton.disabled = false
+          return displayErrors(data.errors)
+        }
+        formElement[0].value = formElement[1].value = formElement[2].value = ''
+        formElement[3].checked = false
+        submitButton.innerHTML = 'Lähetetty - Pistä toinen'
+        submitButton.disabled = true
       })
+  })
+}
+
+const clearErrors = () => {
+  errorNotification.className = 'error hidden'
+  while(errorMessages.firstChild) {
+    errorMessages.firstChild.remove()
+  }
+}
+
+const displayErrors = errors => {
+  errorNotification.className = 'error'
+  const errorArray = Array.isArray(errors)
+    ? errors
+    : [error]
+  errorArray.forEach(error => {
+    const element = document.createElement('div')
+    element.innerHTML = error
+    errorMessages.append(element)
   })
 }
