e380d7ade809d55f799e40e46a6639fec6bb612e
niemisami <sami@nieminen.fi>
2022-09-08 22:20:19 +0300

    Borrow neat canvas animation from the interwebs

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,19 +1,64 @@
-var c = document.getElementById("canvas");
-var ctx = c.getContext("2d");
-var cH;
-var cW;
-var bgColor = "#FF6138";
-var animations = [];
-var circles = [];
+// Borrowed from https://codepen.io/alexzaworski/pen/mEZvrG
+const c = document.getElementById("canvas");
+const ctx = c.getContext("2d");
+let cH;
+let cW;
+let bgColor = "#373f47";
+let animations = [];
+let circles = [];
 
-var colorPicker = (function() {
-  var colors = ["#FF6138", "#B8336A", "#5BC0EB", "#282741"];
-  var index = 0;
-  function next() {
-    index = index++ < colors.length-1 ? index : 0;
+const mult = {
+  d: 2.25,
+  r: 1.5,
+  p: 0.5
+}
+
+// dev-tools
+const sliders = [
+  ['duration', 'd'],
+  ['radius', 'r'],
+  ['particle', 'p']
+]
+sliders.forEach(([key, target, min = 0.1, max = 5]) => {
+  const slider = document.getElementById(`${key}Range`);
+  const output = document.getElementById(`${key}Val`);
+
+  slider.value = scale(mult[target], min, max, 0, 100)
+  output.innerHTML = `${key} ${mult[target]}`
+
+  slider.oninput = function() {
+    mult[target] = round(scale(this.value, 0, 100, min, max))
+    output.innerHTML = `${key} ${mult[target]}`
+  }
+})
+
+function round(value) {
+  return Math.round((value + Number.EPSILON) * 100) / 100
+}
+
+function scale (value, sourceRangeMin, sourceRangeMax, targetRangeMin, targetRangeMax) {
+  var targetRange = targetRangeMax - targetRangeMin;
+  var sourceRange = sourceRangeMax - sourceRangeMin;
+  return (value - sourceRangeMin) * targetRange / sourceRange + targetRangeMin;
+}
+//////////////////
+
+let colorPicker = (function() {
+  let colors = [
+    "#A85751",
+    "#FFB4B4",
+    "#B2A4FF",
+    "#373f47",
+    "#f2b880",
+    "#FFDEB4",
+    "#DBF9F4",
+  ]
+  let index = 0;
+  function next () {
+    index = index++ < colors.length - 1 ? index : 0;
     return colors[index];
   }
-  function current() {
+  function current () {
     return colors[index]
   }
   return {
@@ -22,98 +67,104 @@ var colorPicker = (function() {
   }
 })();
 
-function removeAnimation(animation) {
-  var index = animations.indexOf(animation);
-  if (index > -1) animations.splice(index, 1);
+function removeAnimation (animation) {
+  let index = animations.indexOf(animation);
+  if(index > -1) animations.splice(index, 1);
 }
 
-function calcPageFillRadius(x, y) {
-  var l = Math.max(x - 0, cW - x);
-  var h = Math.max(y - 0, cH - y);
+function calcPageFillRadius (x, y) {
+  let l = Math.max(x - 0, cW - x);
+  let h = Math.max(y - 0, cH - y);
   return Math.sqrt(Math.pow(l, 2) + Math.pow(h, 2));
 }
 
-function addClickListeners() {
+function addClickListeners () {
   document.addEventListener("touchstart", handleEvent);
   document.addEventListener("mousedown", handleEvent);
 };
 
-function handleEvent(e) {
-    if (e.touches) { 
-      e.preventDefault();
-      e = e.touches[0];
+function handleEvent (e) {
+  if(e.touches) {
+    e.preventDefault();
+    e = e.touches[0];
+  }
+  let currentColor = colorPicker.current();
+  let nextColor = colorPicker.next();
+  let targetR = mult.r * calcPageFillRadius(e.pageX, e.pageY);
+  let rippleSize = mult.r * Math.min(200, (cW * .4));
+  let minCoverDuration = mult.d * 750;
+
+  let pageFill = new Circle({
+    x: e.pageX,
+    y: e.pageY,
+    r: 0,
+    fill: nextColor
+  });
+  let fillAnimation = anime({
+    targets: pageFill,
+    r: targetR,
+    duration: mult.d * Math.max(targetR / 2, minCoverDuration),
+    easing: "easeOutQuart",
+    complete: function() {
+      bgColor = pageFill.fill;
+      removeAnimation(fillAnimation);
     }
-    var currentColor = colorPicker.current();
-    var nextColor = colorPicker.next();
-    var targetR = calcPageFillRadius(e.pageX, e.pageY);
-    var rippleSize = Math.min(200, (cW * .4));
-    var minCoverDuration = 750;
-    
-    var pageFill = new Circle({
+  });
+
+  let ripple = new Circle({
+    x: e.pageX,
+    y: e.pageY,
+    r: 0,
+    fill: currentColor,
+    stroke: {
+      width: 3,
+      color: currentColor
+    },
+    opacity: 1
+  });
+  let rippleAnimation = anime({
+    targets: ripple,
+    r: rippleSize,
+    opacity: 0,
+    easing: "easeOutExpo",
+    duration: mult.d * 900,
+    complete: removeAnimation
+  });
+
+  let particles = [];
+  for(let i = 0; i < mult.p * 32; i++) {
+    let particle = new Circle({
       x: e.pageX,
       y: e.pageY,
-      r: 0,
-      fill: nextColor
-    });
-    var fillAnimation = anime({
-      targets: pageFill,
-      r: targetR,
-      duration:  Math.max(targetR / 2 , minCoverDuration ),
-      easing: "easeOutQuart",
-      complete: function(){
-        bgColor = pageFill.fill;
-        removeAnimation(fillAnimation);
-      }
-    });
-    
-    var ripple = new Circle({
-      x: e.pageX,
-      y: e.pageY,
-      r: 0,
       fill: currentColor,
-      stroke: {
-        width: 3,
-        color: currentColor
-      },
+      r: anime.random(24, 48),
       opacity: 1
-    });
-    var rippleAnimation = anime({
-      targets: ripple,
-      r: rippleSize,
-      opacity: 0,
-      easing: "easeOutExpo",
-      duration: 900,
-      complete: removeAnimation
-    });
-    
-    var particles = [];
-    for (var i=0; i<32; i++) {
-      var particle = new Circle({
-        x: e.pageX,
-        y: e.pageY,
-        fill: currentColor,
-        r: anime.random(24, 48)
-      })
-      particles.push(particle);
-    }
-    var particlesAnimation = anime({
-      targets: particles,
-      x: function(particle){
-        return particle.x + anime.random(rippleSize, -rippleSize);
-      },
-      y: function(particle){
-        return particle.y + anime.random(rippleSize * 1.15, -rippleSize * 1.15);
-      },
-      r: 0,
-      easing: "easeOutExpo",
-      duration: anime.random(1000,1300),
-      complete: removeAnimation
-    });
-    animations.push(fillAnimation, rippleAnimation, particlesAnimation);
+    })
+    particles.push(particle);
+  }
+  let particlesAnimation = anime({
+    targets: particles,
+    x: function(particle) {
+      return particle.x + anime.random(rippleSize, -rippleSize);
+    },
+    y: function(particle) {
+      return particle.y + anime.random(rippleSize * 1.15, -rippleSize * 1.15);
+    },
+    r: 0,
+    opacity: 0,
+    easing: "easeOutExpo",
+    duration: mult.d * anime.random(1000, 1300),
+    complete: removeAnimation
+  });
+  animations.push(
+    fillAnimation,
+    rippleAnimation,
+    particlesAnimation
+  );
 }
 
-function extend(a, b){
-  for(var key in b) {
+function extend (a, b) {
+  for(let key in b) {
     if(b.hasOwnProperty(key)) {
       a[key] = b[key];
     }
@@ -121,7 +172,7 @@ function extend(a, b){
   return a;
 }
 
-var Circle = function(opts) {
+let Circle = function(opts) {
   extend(this, opts);
 }
 
@@ -129,12 +180,12 @@ Circle.prototype.draw = function() {
   ctx.globalAlpha = this.opacity || 1;
   ctx.beginPath();
   ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
-  if (this.stroke) {
+  if(this.stroke) {
     ctx.strokeStyle = this.stroke.color;
     ctx.lineWidth = this.stroke.width;
     ctx.stroke();
   }
-  if (this.fill) {
+  if(this.fill) {
     ctx.fillStyle = this.fill;
     ctx.fill();
   }
@@ -142,7 +193,7 @@ Circle.prototype.draw = function() {
   ctx.globalAlpha = 1;
 }
 
-var animate = anime({
+let animate = anime({
   duration: Infinity,
   update: function() {
     ctx.fillStyle = bgColor;
@@ -155,7 +206,7 @@ var animate = anime({
   }
 });
 
-var resizeCanvas = function() {
+let resizeCanvas = function() {
   cW = window.innerWidth;
   cH = window.innerHeight;
   c.width = cW * devicePixelRatio;
@@ -163,45 +214,37 @@ var resizeCanvas = function() {
   ctx.scale(devicePixelRatio, devicePixelRatio);
 };
 
-(function init() {
+(function init () {
   resizeCanvas();
-  if (window.CP) {
-    // CodePen's loop detection was causin' problems
-    // and I have no idea why, so...
-    window.CP.PenTimer.MAX_TIME_IN_LOOP_WO_EXIT = 6000; 
-  }
   window.addEventListener("resize", resizeCanvas);
   addClickListeners();
-  if (!!window.location.pathname.match(/fullcpgrid/)) {
-    startFauxClicking();
-  }
   handleInactiveUser();
 })();
 
-function handleInactiveUser() {
-  var inactive = setTimeout(function(){
-    fauxClick(cW/2, cH/2);
-  }, 2000);
-  
-  function clearInactiveTimeout() {
+function handleInactiveUser () {
+  let inactive = setTimeout(function() {
+    fauxClick(anime.random(cW * .2, cW * .8), anime.random(cH * .2, cH * .8));
+  }, 4000);
+
+  function clearInactiveTimeout () {
     clearTimeout(inactive);
     document.removeEventListener("mousedown", clearInactiveTimeout);
     document.removeEventListener("touchstart", clearInactiveTimeout);
   }
-  
+
   document.addEventListener("mousedown", clearInactiveTimeout);
   document.addEventListener("touchstart", clearInactiveTimeout);
 }
 
-function startFauxClicking() {
-  setTimeout(function(){
-    fauxClick(anime.random( cW * .2, cW * .8), anime.random(cH * .2, cH * .8));
+function startFauxClicking () {
+  setTimeout(function() {
+    fauxClick(anime.random(cW * .2, cW * .8), anime.random(cH * .2, cH * .8));
     startFauxClicking();
-  }, anime.random(200, 900));
+  }, anime.random(2000, 9000));
 }
 
-function fauxClick(x, y) {
-  var fauxClick = new Event("mousedown");
+function fauxClick (x, y) {
+  let fauxClick = new Event("mousedown");
   fauxClick.pageX = x;
   fauxClick.pageY = y;
   document.dispatchEvent(fauxClick);
